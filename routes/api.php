c54c7ece00bba67accbf13c68709210b4d6f3d37
finn <finnnxptrix@gmail.com>
2025-09-29 11:28:59 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\StudentController;
-use App\Http\Controllers\Api\AttendanceController;
 use App\Http\Middleware\SyncApiAuth;
 use App\Http\Controllers\Api\GenericSyncController;
 
@@ -16,8 +14,6 @@ Route::post('/sync/{entity}', [GenericSyncController::class, 'handleSync'])
      ->middleware(SyncApiAuth::class);
 
 // Endpoint lain untuk aplikasi Anda (jika ada)
-Route::apiResource('students', StudentController::class)->names('api.students');
-Route::post('/attendance', [AttendanceController::class, 'store'])->name('attendance.store');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
