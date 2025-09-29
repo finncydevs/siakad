11d458559b0e5ce0aaaa38efe88b41d4aa1b707c
finn <finnnxptrix@gmail.com>
2025-09-29 14:23:05 +0700

    woi

--- tests/Feature/Auth/PasswordConfirmationTest.php
+++ tests/Feature/Auth/PasswordConfirmationTest.php
@@ -1,6 +1,5 @@
 <?php
 
-<<<<<<< HEAD
 namespace Tests\Feature\Auth;
 
 use App\Models\User;
@@ -43,35 +42,3 @@ class PasswordConfirmationTest extends TestCase
         $response->assertSessionHasErrors();
     }
 }
-=======
-use App\Models\User;
-
-test('confirm password screen can be rendered', function () {
-    $user = User::factory()->create();
-
-    $response = $this->actingAs($user)->get('/confirm-password');
-
-    $response->assertStatus(200);
-});
-
-test('password can be confirmed', function () {
-    $user = User::factory()->create();
-
-    $response = $this->actingAs($user)->post('/confirm-password', [
-        'password' => 'password',
-    ]);
-
-    $response->assertRedirect();
-    $response->assertSessionHasNoErrors();
-});
-
-test('password is not confirmed with invalid password', function () {
-    $user = User::factory()->create();
-
-    $response = $this->actingAs($user)->post('/confirm-password', [
-        'password' => 'wrong-password',
-    ]);
-
-    $response->assertSessionHasErrors();
-});
->>>>>>> 7a2424f8a67bc4281ef5e9a434b27e005fb8a7ed
