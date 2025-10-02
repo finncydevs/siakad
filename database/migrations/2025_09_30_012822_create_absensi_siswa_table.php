aaa34bd790ad32c5ed7f28075221eae871f25447
izar1008 <kn1707412@gmail.com>
2025-10-02 15:25:52 +0700

    -

--- database/migrations/2025_09_30_012822_create_absensi_siswa_table.php
+++ database/migrations/2025_09_30_012822_create_absensi_siswa_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('absensi_siswa', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('jadwal_id')->constrained('jadwal_pelajaran')->onDelete('cascade');
+            // $table->foreignId('jadwal_id')->constrained('jadwal_pelajaran')->onDelete('cascade');
             $table->foreignId('siswa_id')->constrained('siswas')->onDelete('cascade');
             $table->date('tanggal');
             $table->enum('status', ['Hadir', 'Sakit', 'Izin', 'Alfa']);
