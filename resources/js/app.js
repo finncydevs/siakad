e52eeb5cc4688ee994c97454621ac48dde26fb12
izar1008 <kn1707412@gmail.com>
2025-10-03 13:44:01 +0700

    -

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,8 @@
-// Import vendor Sneat
-import '../sneat/assets/vendor/libs/jquery/jquery.js';
-import '../sneat/assets/vendor/libs/popper/popper.js';
-import '../sneat/assets/vendor/js/bootstrap.js';
-import '../sneat/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js';
-import '../sneat/assets/vendor/js/menu.js';
-import '../sneat/assets/vendor/libs/apex-charts/apexcharts.js';
-import '../sneat/assets/js/main.js';
-import '../sneat/assets/js/dashboards-analytics.js';
-import '../sneat/assets/js/ui-modals.js';
-import '../sneat/assets/js/ui-toasts.js';
 
-// Laravel bootstrap
 import './bootstrap';
 
-// AlpineJS
 import Alpine from 'alpinejs';
+
 window.Alpine = Alpine;
+
 Alpine.start();
