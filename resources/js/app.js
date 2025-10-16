6bac1efc8b4ffd44455d80b2c2cc3dec37262b25
izar1008 <kn1707412@gmail.com>
2025-10-17 06:02:30 +0700

    absen

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,25 +1,29 @@
-import './bootstrap'
-import Alpine from 'alpinejs'
+import './bootstrap';
+import Alpine from 'alpinejs';
 
-window.Alpine = Alpine
-Alpine.start()
+window.Alpine = Alpine;
+Alpine.start();
 
-// Sneat Vendor JS
-import '~sneat/vendor/libs/jquery/jquery.js'
-import '~sneat/vendor/libs/popper/popper.js'
-import '~sneat/vendor/js/bootstrap.js'
-import '~sneat/vendor/libs/perfect-scrollbar/perfect-scrollbar.js'
+// Sneat Vendor JS - Library dasar
+import '~sneat/vendor/libs/jquery/jquery.js';
+import '~sneat/vendor/libs/popper/popper.js';
+import '~sneat/vendor/js/bootstrap.js';
+import '~sneat/vendor/libs/perfect-scrollbar/perfect-scrollbar.js';
 
 // =================================================================
-// ==> TAMBAHKAN BARIS INI, INI ADALAH KUNCINYA <==
-import '~sneat/vendor/js/helpers.js'
+// ==> HELPER HARUS DI-IMPORT SEBELUM MENU DAN MAIN <==
+// File ini mendefinisikan objek 'config' yang dibutuhkan oleh script lain.
+import '~sneat/vendor/js/helpers.js';
 // =================================================================
 
-import '~sneat/vendor/js/menu.js'
-import '~sneat/vendor/libs/apex-charts/apexcharts.js'
+// Script yang bergantung pada 'helpers.js'
+import '~sneat/vendor/js/menu.js';
 
-// Sneat Core JS
-import '~sneat/js/main.js'
-import '~sneat/js/dashboards-analytics.js'
-import '~sneat/js/ui-modals.js'
-import '~sneat/js/ui-toasts.js'
+// Sneat Core JS - Script utama tema
+import '~sneat/js/main.js';
+
+// Page-specific JS - Bisa diletakkan setelah script utama
+import '~sneat/vendor/libs/apex-charts/apexcharts.js';
+import '~sneat/js/dashboards-analytics.js';
+import '~sneat/js/ui-modals.js';
+import '~sneat/js/ui-toasts.js';
