3087192dec63356491f63afc83fb7e2f4204576d
finn <finnnxptrix@gmail.com>
2025-10-17 14:08:39 +0700

    commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
-// Corrected PostCSS config
 export default {
     plugins: {
-        // Change '@tailwindcss/postcss' to 'tailwindcss'
-        tailwindcss: {},
+        "@tailwindcss/postcss": {},
         autoprefixer: {},
     },
 };
