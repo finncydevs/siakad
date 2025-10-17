d46f264df0f161e59a3a87a177fdc6a21a079a4c
finn <finnnxptrix@gmail.com>
2025-10-17 14:12:28 +0700

    bakso:

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,17 @@ import path from 'path'
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
+      input: ['resources/css/app.css', 'resources/js/app.js',
+
+             "resources/css/app.css",
+                "resources/js/app.js",
+
+                "resources/sneat/assets/vendor/fonts/boxicons.css", // (Jika ini yang Anda perbaiki sebelumnya)
+                "resources/sneat/assets/vendor/css/core.css", // ⬅️ INI YANG BARU
+                "resources/sneat/assets/vendor/css/theme-default.css",
+                "resources/sneat/assets/css/demo.css",
+                "resources/sneat/assets/vendor/css/pages/page-auth.css",
+      ],
       refresh: true,
     }),
   ],
